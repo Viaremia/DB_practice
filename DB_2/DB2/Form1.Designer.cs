26938e98fead3ae3653c16ef465e26d099d22298
Viaremia <viaremia@gmail.com>
2020-10-08 20:08:55 +0300

    Query generator

---
+++ DB_2/DB2/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace DB2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxQueryType = new System.Windows.Forms.ComboBox();
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.connectDBToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.createToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.comboBoxTable = new System.Windows.Forms.ComboBox();
+            this.comboBoxTableField = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBoxQueryType
+            // 
+            this.comboBoxQueryType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxQueryType.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBoxQueryType.FormattingEnabled = true;
+            this.comboBoxQueryType.Location = new System.Drawing.Point(27, 163);
+            this.comboBoxQueryType.Name = "comboBoxQueryType";
+            this.comboBoxQueryType.Size = new System.Drawing.Size(200, 24);
+            this.comboBoxQueryType.TabIndex = 0;
+            this.comboBoxQueryType.DropDown += new System.EventHandler(this.QueryTypeDrpDwn);
+            this.comboBoxQueryType.SelectedValueChanged += new System.EventHandler(this.QueryTypeSlctValChenged);
+            // 
+            // richTextBox
+            // 
+            this.richTextBox.Location = new System.Drawing.Point(12, 40);
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.Size = new System.Drawing.Size(776, 96);
+            this.richTextBox.TabIndex = 1;
+            this.richTextBox.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(27, 193);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(737, 39);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.GenerateBtnClick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.connectDBToolStripMenuItem,
+            this.createToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // connectDBToolStripMenuItem
+            // 
+            this.connectDBToolStripMenuItem.Name = "connectDBToolStripMenuItem";
+            this.connectDBToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.connectDBToolStripMenuItem.Text = "ConnectDB";
+            this.connectDBToolStripMenuItem.Click += new System.EventHandler(this.ConnectDBClick);
+            // 
+            // createToolStripMenuItem
+            // 
+            this.createToolStripMenuItem.Name = "createToolStripMenuItem";
+            this.createToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.createToolStripMenuItem.Text = "Create";
+            this.createToolStripMenuItem.Click += new System.EventHandler(this.CreateClick);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.SaveClicl);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.OpenClick);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.ExitClick);
+            // 
+            // comboBoxTable
+            // 
+            this.comboBoxTable.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxTable.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBoxTable.FormattingEnabled = true;
+            this.comboBoxTable.Location = new System.Drawing.Point(297, 163);
+            this.comboBoxTable.Name = "comboBoxTable";
+            this.comboBoxTable.Size = new System.Drawing.Size(200, 24);
+            this.comboBoxTable.TabIndex = 0;
+            this.comboBoxTable.DropDown += new System.EventHandler(this.TableDrpDwn);
+            // 
+            // comboBoxTableField
+            // 
+            this.comboBoxTableField.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxTableField.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBoxTableField.FormattingEnabled = true;
+            this.comboBoxTableField.Location = new System.Drawing.Point(564, 163);
+            this.comboBoxTableField.Name = "comboBoxTableField";
+            this.comboBoxTableField.Size = new System.Drawing.Size(200, 24);
+            this.comboBoxTableField.TabIndex = 0;
+            this.comboBoxTableField.DropDown += new System.EventHandler(this.TableFieldDrpDwn);
+            this.comboBoxTableField.SelectedValueChanged += new System.EventHandler(this.TableFieldSlctValChenged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(35, 144);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Query type";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(310, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Table";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(576, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Table field";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Maroon;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button2.Location = new System.Drawing.Point(713, 27);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 10);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "RebButton";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.RedButtonClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 244);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox);
+            this.Controls.Add(this.comboBoxTableField);
+            this.Controls.Add(this.comboBoxTable);
+            this.Controls.Add(this.comboBoxQueryType);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "DB2";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxQueryType;
+        private System.Windows.Forms.RichTextBox richTextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem createToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ComboBox comboBoxTable;
+        private System.Windows.Forms.ComboBox comboBoxTableField;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ToolStripMenuItem connectDBToolStripMenuItem;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ DB_2/DB2/Form1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using System.IO;
+using System.Data.OleDb;
+
+namespace DB2
+{
+    public partial class Form1 : Form
+    {
+        private OleDbConnection connection = null;
+        public Form1()
+        {
+            InitializeComponent();
+            FormLoad();
+        }
+
+        private void ConnectDBClick(object sender, EventArgs e)
+        {
+            string connectionString = $"Provider = Microsoft.Jet.OLEDB.4.0; Data Source = ";
+
+            using (OpenFileDialog fd = new OpenFileDialog())
+            {
+                fd.Filter = "Access file(*.mdb)|*.mdb";
+                if (fd.ShowDialog() == DialogResult.Cancel)
+                    return;
+
+                connectionString += fd.FileName + ";";
+            }
+
+            connection = new OleDbConnection(connectionString);
+            connection.Open();
+        }
+
+        private void CreateClick(object sender, EventArgs e)
+        {
+            richTextBox.Visible = true;
+        }
+
+        private void OpenClick(object sender, EventArgs e)
+        {
+            using (StreamReader sr = new StreamReader("Query.txt"))
+            {
+                richTextBox.Text = sr.ReadToEnd();
+            }
+        }
+
+        private void SaveClicl(object sender, EventArgs e)
+        {
+            using (StreamWriter sw = new StreamWriter("Query.txt", false))
+            {
+                sw.WriteLine(richTextBox.Text);
+            }
+        }
+
+        private void GenerateBtnClick(object sender, EventArgs e)
+        {
+            GeneratorStage1();
+            GeneratorStage2();
+            int index = richTextBox.Text.Length - 2;
+            char c = richTextBox.Text[index];
+            if (richTextBox.Text[index] == ',')
+            {
+                richTextBox.Text = richTextBox.Text.Remove(index, 1);
+                richTextBox.Text += ";";
+                return;
+            }
+
+            if (richTextBox.Text[index] != ';')
+                richTextBox.Text += ";";
+        }
+
+        private void GeneratorStage1()
+        {
+            string[] keyWords = new string[] { "FROM", "UPDATE", "TABLE" };
+            string tempText = richTextBox.Text;
+            foreach (string s in keyWords)
+            {
+                int i = tempText.IndexOf(s, 0);
+                if (i == -1)
+                    goto point1;
+                i += s.Length;
+                if (s == "TABLE")
+                {
+                    tempText = tempText.Insert(i, " [Table_name]");
+                    goto point1;
+                }
+                tempText = tempText.Insert(i, $" [{comboBoxTable.Text}]");
+            point1:
+                richTextBox.Text = tempText;
+            }
+        }
+
+        private void GeneratorStage2()
+        {
+            string[] keyWords = new string[] { "SET", "WHERE" };
+            string tempText = richTextBox.Text;
+            
+            foreach(string s in keyWords)
+            {
+                int i = tempText.IndexOf(s, 0);
+                if (i == -1)
+                    goto point1;
+                i += s.Length;
+                int index = 0;
+                if (s == "SET")
+                {
+                     index = tempText.IndexOf("WHERE", 0);
+                    if (index != -1)
+                        index = index - i;
+                }
+
+                string subStr = tempText.Substring(i, index);
+                string[] spliter = subStr.Split(' ');
+                subStr = null;
+                for (int j = 0; j < spliter.Length; j++)
+                {
+                    if (spliter[j] != "")
+                        tempText = tempText.Replace(spliter[j], spliter[j] + " = \'your_text\',");
+                }
+            point1:
+                richTextBox.Text = tempText;
+            }
+        }
+        // изм значения в полях таблицы
+        private void TableFieldSlctValChenged(object sender, EventArgs e)
+        {
+            if (richTextBox.Visible == false || comboBoxTableField.Text == "")
+                return;
+            if (comboBoxTableField.Text == "All")
+            {
+                richTextBox.Text += "* ";
+                return;
+            }
+            richTextBox.Text += comboBoxTableField.Text + " "; 
+        }
+
+        // изм значения в запросах
+        private void QueryTypeSlctValChenged(object sender, EventArgs e)
+        {
+            if (richTextBox.Visible == false || comboBoxQueryType.Text == "")
+                return;
+
+            richTextBox.Text += comboBoxQueryType.Text + " ";
+        }
+
+        // выпадение запросов
+        private void QueryTypeDrpDwn(object sender, EventArgs e)
+        {
+            try
+            {
+                comboBoxQueryType.Items.Clear();
+                using (StreamReader sr = new StreamReader("QueryTypes.txt"))
+                {
+                    string val = null;
+                    while ((val = sr.ReadLine()) != null)
+                        comboBoxQueryType.Items.Add(val);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        // выпадение таблицы
+        private void TableDrpDwn(object sender, EventArgs e)
+        {
+            if (connection == null)
+                return;
+
+            comboBoxTable.Items.Clear();
+            DataTable tables = connection.GetSchema("Tables", new string[] { null, null, null, "TABLE" });
+            foreach (DataRow row in tables.Rows)
+            {
+                string tableName = row["TABLE_NAME"].ToString();
+                comboBoxTable.Items.Add(tableName);
+            }
+        }
+
+        // выпадение полей таблицы
+        private void TableFieldDrpDwn(object sender, EventArgs e)
+        {
+            if (comboBoxTable.Text == "")
+                return;
+
+            comboBoxTableField.Items.Clear();
+            using (OleDbCommand command = new OleDbCommand($"SELECT * FROM {comboBoxTable.Text} WHERE 1 = 0", connection))
+            {
+                OleDbDataReader reader = command.ExecuteReader();
+                DataTable table = reader.GetSchemaTable();
+                foreach (DataRow t in table.Rows)
+                {
+                    string rowName = t.Field<string>("ColumnName");
+                    comboBoxTableField.Items.Add(rowName);
+                }
+                comboBoxTableField.Items.Add("All");
+                reader.Close();
+            }
+        }
+
+
+        private void RedButtonClick(object sender, EventArgs e) 
+        {
+            richTextBox.Clear();
+        }
+
+        private void ExitClick(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void FormClose(object sender, FormClosingEventArgs e) 
+        {
+            connection.Close();
+        }
+
+        private void FormLoad()
+        {
+            richTextBox.Visible = false;
+        }
+        
+    }
+}
