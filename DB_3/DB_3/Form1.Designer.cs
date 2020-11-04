ccbc10d311e7f29d98629307675013d3707c3c00
Viaremia <viaremia@gmail.com>
2020-11-04 23:02:40 +0200

    Interface for working with DB

---
+++ DB_3/DB_3/Form1.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace DB_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.connectDBToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.textBoxWhere = new System.Windows.Forms.TextBox();
+            this.comboBoxTables = new System.Windows.Forms.ComboBox();
+            this.textBoxValues = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.buttonLast = new System.Windows.Forms.Button();
+            this.buttonFirst = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.connectDBToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(958, 26);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // connectDBToolStripMenuItem
+            // 
+            this.connectDBToolStripMenuItem.Font = new System.Drawing.Font("Sylfaen", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.connectDBToolStripMenuItem.Name = "connectDBToolStripMenuItem";
+            this.connectDBToolStripMenuItem.Size = new System.Drawing.Size(85, 22);
+            this.connectDBToolStripMenuItem.Text = "Connect DB";
+            this.connectDBToolStripMenuItem.Click += new System.EventHandler(this.ConnectDBClick);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Font = new System.Drawing.Font("Sylfaen", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(41, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.ExitClick);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.BackgroundColor = System.Drawing.SystemColors.ControlLight;
+            this.dataGridView.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.Sunken;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.GridColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.dataGridView.Location = new System.Drawing.Point(12, 96);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(799, 291);
+            this.dataGridView.TabIndex = 1;
+            // 
+            // textBoxWhere
+            // 
+            this.textBoxWhere.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxWhere.Location = new System.Drawing.Point(131, 37);
+            this.textBoxWhere.Name = "textBoxWhere";
+            this.textBoxWhere.Size = new System.Drawing.Size(209, 22);
+            this.textBoxWhere.TabIndex = 2;
+            // 
+            // comboBoxTables
+            // 
+            this.comboBoxTables.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxTables.FormattingEnabled = true;
+            this.comboBoxTables.Location = new System.Drawing.Point(131, 65);
+            this.comboBoxTables.Name = "comboBoxTables";
+            this.comboBoxTables.Size = new System.Drawing.Size(209, 21);
+            this.comboBoxTables.TabIndex = 3;
+            this.comboBoxTables.DropDown += new System.EventHandler(this.TablesDropDown);
+            this.comboBoxTables.DropDownClosed += new System.EventHandler(this.TablesDropDownClosed);
+            // 
+            // textBoxValues
+            // 
+            this.textBoxValues.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxValues.Location = new System.Drawing.Point(12, 393);
+            this.textBoxValues.Name = "textBoxValues";
+            this.textBoxValues.Size = new System.Drawing.Size(799, 22);
+            this.textBoxValues.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button1.Font = new System.Drawing.Font("Sylfaen", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(817, 96);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Selection";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.SelectionBtnClick);
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button2.Font = new System.Drawing.Font("Sylfaen", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(817, 140);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(129, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Delete";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.DeleteBtnClick);
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button3.Font = new System.Drawing.Font("Sylfaen", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(817, 186);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(129, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Add";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.AddBtnClick);
+            // 
+            // button4
+            // 
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button4.Font = new System.Drawing.Font("Sylfaen", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(817, 244);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(129, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Update";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.UpdateBtnclick);
+            // 
+            // buttonLast
+            // 
+            this.buttonLast.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.buttonLast.Font = new System.Drawing.Font("Sylfaen", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonLast.Location = new System.Drawing.Point(655, 425);
+            this.buttonLast.Name = "buttonLast";
+            this.buttonLast.Size = new System.Drawing.Size(75, 23);
+            this.buttonLast.TabIndex = 4;
+            this.buttonLast.Text = "Last";
+            this.buttonLast.UseVisualStyleBackColor = true;
+            this.buttonLast.Click += new System.EventHandler(this.LastBtnClick);
+            // 
+            // buttonFirst
+            // 
+            this.buttonFirst.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.buttonFirst.Font = new System.Drawing.Font("Sylfaen", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonFirst.Location = new System.Drawing.Point(736, 424);
+            this.buttonFirst.Name = "buttonFirst";
+            this.buttonFirst.Size = new System.Drawing.Size(75, 23);
+            this.buttonFirst.TabIndex = 4;
+            this.buttonFirst.Text = "First";
+            this.buttonFirst.UseVisualStyleBackColor = true;
+            this.buttonFirst.Click += new System.EventHandler(this.FirstBtnClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Sylfaen", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(23, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 19);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Tables names";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Sylfaen", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(23, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 19);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "\"Where\" query";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label3.Font = new System.Drawing.Font("Sylfaen", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(817, 396);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(15, 19);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "?";
+            this.label3.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Question1MouseClick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label4.Font = new System.Drawing.Font("Sylfaen", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(346, 40);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(15, 19);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "?";
+            this.label4.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Question2MouseClick);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label5.Font = new System.Drawing.Font("Sylfaen", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(931, 212);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(15, 19);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "?";
+            this.label5.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Question3MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(958, 459);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonFirst);
+            this.Controls.Add(this.buttonLast);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBoxTables);
+            this.Controls.Add(this.textBoxValues);
+            this.Controls.Add(this.textBoxWhere);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem connectDBToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.TextBox textBoxWhere;
+        private System.Windows.Forms.ComboBox comboBoxTables;
+        private System.Windows.Forms.TextBox textBoxValues;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button buttonLast;
+        private System.Windows.Forms.Button buttonFirst;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ DB_3/DB_3/Form1.cs
@@ -0,0 +1,323 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace DB_3
+{
+    public partial class Form1 : Form
+    {
+        OleDbConnection connection = null;
+        DataTable tables = null;
+        List<string> columnsDateType = new List<string> { };
+        List<string> columnsNames = new List<string> { };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void DeleteBtnClick(object sender, EventArgs e)
+        {
+            if (connection == null || connection.State == ConnectionState.Closed)
+                return;
+
+            foreach (DataGridViewRow row in dataGridView.SelectedRows)
+            {
+                int id = int.Parse(row.Cells[0].Value.ToString());
+                dataGridView.Rows.RemoveAt(row.Index);
+
+                using (OleDbCommand command = new OleDbCommand($"DELETE FROM [{comboBoxTables.Text}] WHERE Id = {id}", connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        private void UpdateBtnclick(object sender, EventArgs e)
+        {
+            if (connection == null || connection.State == ConnectionState.Closed)
+                return;
+            string query = $"UPDATE {comboBoxTables.Text} SET ";
+            query += textBoxValues.Text;
+            query += " WHERE " + textBoxWhere.Text + ";";
+
+            using (OleDbCommand command = new OleDbCommand(query, connection))
+            {
+                try
+                {
+                    command.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Attention!");
+                }
+            }
+
+            string slct = $"SELECT * FROM {comboBoxTables.Text}";
+            DataLoad(slct);
+        }
+
+        private void AddBtnClick(object sender, EventArgs e)
+        {
+            if (connection == null || connection.State == ConnectionState.Closed)
+                return;
+
+            string colNames = null;
+            for (int i = 0; i != columnsNames.Count; i++)
+            {
+                colNames += $"[{columnsNames[i]}]";
+                if (columnsNames.Count - i != 1)
+                    colNames += ",";
+            }
+
+            string query = $"INSERT INTO {comboBoxTables.Text} ({colNames}) VALUES (";
+            InsertFunc(ref query);
+            query = query.Remove(query.Length - 2);
+            query += ");";
+
+            using (OleDbCommand command = new OleDbCommand(query, connection))
+            {
+                try
+                {
+                    int i = command.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void InsertFunc(ref string query)
+        { 
+            for (int i = 0; i != columnsNames.Count; i++)
+            {
+                string cell = null;
+                try
+                {
+                    cell = dataGridView[columnsNames[i], dataGridView.RowCount - 2].Value.ToString();
+                }
+                catch (NullReferenceException ex)
+                {
+                    if (columnsNames[i] == "Id")
+                    {
+                        query += dataGridView.RowCount - 1 + ", ";
+                        continue;
+                    }
+                    cell = null;
+                    query += "NULL, ";
+                    continue;
+                }
+
+                if (columnsDateType[i] == "Int32" || columnsDateType[i] == "Double")
+                {
+                    cell = cell.Replace(',', '.');
+                    query += cell + ", ";
+                }
+                else if (columnsDateType[i] == "DateTime")
+                {
+                    string[] temp = null;
+                    string tempStr = null;
+
+                    temp = cell.Split('.');
+
+                    tempStr = temp[0];
+                    temp[0] = temp[1];
+                    temp[1] = tempStr;
+
+                    cell = null;
+                    cell += "#";
+                    for (int j = 0; j < 2; j++)
+                    {
+                        cell += temp[j] + "/";
+                    }
+                    cell += temp[2] + "#, ";
+                    query += cell;
+                }
+                else if (columnsDateType[i] == "Boolean")
+                    query += cell + ", ";
+                else
+                    query += "'" + cell + "', ";
+            }
+        }
+
+        private void SelectionBtnClick(object sender, EventArgs e)
+        {
+            if (connection == null || connection.State == ConnectionState.Closed)
+                return;
+
+            string query = $"SELECT * FROM {comboBoxTables.Text}";
+
+            if (textBoxWhere.Text != "")
+                query += $" WHERE {textBoxWhere.Text};";
+            else
+                query += ";";
+             
+            DataLoad(query);
+                    
+        }
+
+        private void LastBtnClick(object sender, EventArgs e)
+        {
+            try
+            {
+                dataGridView.ClearSelection();
+                int count = dataGridView.Rows.Count - 2;
+                dataGridView.Rows[count].Selected = true;
+            }
+            catch 
+            {
+                return;
+            }
+            
+        }
+
+        private void FirstBtnClick(object sender, EventArgs e)
+        {
+            try
+            {
+                dataGridView.ClearSelection();
+                dataGridView.Rows[0].Selected = true;
+            }
+            catch
+            {
+                return;
+            }
+        }
+
+        private void TablesDropDownClosed(object sender, EventArgs e)
+        {
+            dataGridView.Columns.Clear();
+            using (OleDbCommand command = new OleDbCommand($"SELECT * FROM {comboBoxTables.Text} WHERE 1 = 0", connection))
+            {
+                OleDbDataReader reader = null;
+                DataTable table = null;
+                try
+                {
+                    reader = command.ExecuteReader();
+                    table = reader.GetSchemaTable();
+                    
+                }
+                catch
+                {
+                    return;
+                }
+                foreach (DataRow row in table.Rows)
+                {
+                    string columnName = row.Field<string>("ColumnName");
+                    Type columnDT = row.Field<Type>("DataType");
+                    dataGridView.Columns.Add(columnName.ToLower(), columnName);
+                    columnsNames.Add(columnName);
+                    columnsDateType.Add(columnDT.Name);
+                }
+            }
+
+            DataLoad($"SELECT * FROM {comboBoxTables.Text};");
+        }
+
+        private void DataLoad(string query)
+        {
+            using (OleDbCommand command = new OleDbCommand(query, connection))
+            {
+                OleDbDataReader reader = null;
+                try
+                {
+                    reader = command.ExecuteReader();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Attention!");
+                    return;
+                }
+                dataGridView.Rows.Clear();
+                while (reader.Read())
+                {
+                    List<string> tempList = new List<string> { };
+                    int count = reader.FieldCount;
+                    for (int i = 0; i != count; i++)
+                        tempList.Add(reader[i].ToString());
+                    dataGridView.Rows.Add(tempList.ToArray());
+                }
+                dataGridView.Sort(dataGridView.Columns[0], ListSortDirection.Ascending);
+            }
+        }
+
+        private void TablesDropDown(object sender, EventArgs e)
+        {
+            comboBoxTables.Items.Clear();
+            try
+            {
+                tables = connection.GetSchema("Tables", new string[] { null, null, null, "TABLE" });
+            }
+            catch
+            {
+                return;
+            }
+            foreach (DataRow row in tables.Rows)
+            {
+                string tableName = row["TABLE_NAME"].ToString();
+                comboBoxTables.Items.Add(tableName);
+            }
+        }
+
+        private void ConnectDBClick(object sender, EventArgs e)
+        {
+            string connectionString = "Provider = Microsoft.ACE.OLEDB.12.0; Data Source = ";
+
+            using (OpenFileDialog fd = new OpenFileDialog())
+            {
+                fd.Filter = "Access file (*.accdb)|*.accdb";
+                if (fd.ShowDialog() == DialogResult.Cancel)
+                    return;
+                connectionString += fd.FileName + ";";
+            }
+
+            if (connection != null && connection.State == ConnectionState.Open)
+            {
+                connection.Close();
+                connection.ConnectionString = connectionString;
+            }
+            else
+                connection = new OleDbConnection(connectionString);
+            connection.Open();
+                
+        }
+
+        private void FormClose(object sender, FormClosingEventArgs e)
+        {
+            connection.Close();
+        }
+
+        private void ExitClick(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void Question1MouseClick(object sender, MouseEventArgs e)
+        {
+            MessageBoxIcon icon = MessageBoxIcon.Information;
+            MessageBoxButtons button = MessageBoxButtons.OK;
+            MessageBox.Show("New values. Examples of use:\n1. [Table_name] = 'something'\n" +
+                "2. [Table_name] = something (without quotes for numbers)\n", "How it work?", button, icon);
+        }
+
+        private void Question2MouseClick(object sender, MouseEventArgs e)
+        {
+            MessageBoxIcon icon = MessageBoxIcon.Information;
+            MessageBoxButtons button = MessageBoxButtons.OK;
+            MessageBox.Show("Your condition. Examples of use:\n1. [Table_name] = 'something'\n" +
+                "2. [Table_name] = something (without quotes for numbers)\n" +
+                "3. [Table_name] = 'something' AND [Table_name] = 'something'", "How it work?", button, icon);
+        }
+
+        private void Question3MouseClick(object sender, MouseEventArgs e)
+        {
+            MessageBoxIcon icon = MessageBoxIcon.Information;
+            MessageBoxButtons button = MessageBoxButtons.OK;
+            MessageBox.Show("For inserting of new values, add it in the last row of data grid. And click add.", "How it work?", button, icon);
+        }
+    }
+}
